4ec314089908584e07332292b211c792540e22a7
Hubert Łępicki <hubert.lepicki@amberbit.com>
2013-08-29 14:55:12 +0200

    Update dishes_controller.rb

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,7 +1,9 @@
 class DishesController < ApplicationController
-  
+  # index
   def autocomplete
     @dishes=Dish.autocomplete(params[:term].downcase)
     render json: @dishes.map {|r| r.name }
   end
+  
+  # create, edit, update should be here
 end
